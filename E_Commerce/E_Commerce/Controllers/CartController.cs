9862957cc07fbe866e624b6a53534b2aade2de4b
OsamaAlzaghal <osama.alzaghal@gmail.com>
2022-05-30 18:19:04 +0300

    Updated controllers

--- E_Commerce/E_Commerce/Controllers/CartController.cs
+++ E_Commerce/E_Commerce/Controllers/CartController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,14 @@ namespace E_Commerce.Controllers
 {
     public class CartController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Cart()
         {
-            return View();
+            List<Cart> carts = new List<Cart>();
+            carts.Add(new Cart { ID = 1, UserID = 1 });
+            carts.Add(new Cart { ID = 2, UserID = 2 });
+            carts.Add(new Cart { ID = 3, UserID = 3 });
+            carts.Add(new Cart { ID = 4, UserID = 4 });
+            return View(carts);
         }
     }
 }

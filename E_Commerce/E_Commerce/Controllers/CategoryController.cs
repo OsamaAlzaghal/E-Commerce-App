b11dd7b544a1c17421fbefcfacdaec85bbc9abc8
OsamaAlzaghal <osama.alzaghal@gmail.com>
2022-06-01 13:37:20 +0300

    Shared layout + Add Category form

--- E_Commerce/E_Commerce/Controllers/CategoryController.cs
+++ E_Commerce/E_Commerce/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using E_Commerce.Models;
+﻿using E_Commerce.Data;
+using E_Commerce.Models;
+using E_Commerce.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,6 +11,7 @@ namespace E_Commerce.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ICategory _category;
         public IActionResult List()
         {
             List<Category> categories = new List<Category>();
@@ -17,6 +20,22 @@ namespace E_Commerce.Controllers
             categories.Add(new Category { ID = 3, Name = "Accessories", Description = "Good"});
             categories.Add(new Category { ID = 4, Name = "Sound System", Description = "Good"});
             return View(categories);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                //_category.CreateCategory(category);
+                return Content("You have successfully added a category ! Name: " + category.Name + " Description: " + category.Description);
+            }
+            return View(category);
         }
     }
 }

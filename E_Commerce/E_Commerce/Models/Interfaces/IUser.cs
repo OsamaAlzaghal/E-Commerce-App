e03394e6f5dad6e4805e2c2f82a93357085e8339
LaithAlamat <laithalamat4@gmail.com>
2022-06-01 19:03:41 +0300

    Partially done

--- E_Commerce/E_Commerce/Models/Interfaces/IUser.cs
+++ E_Commerce/E_Commerce/Models/Interfaces/IUser.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace E_Commerce.Models.Interfaces
 {
-    interface IUser
+    public interface IUser
     {
         public Task<List<User>> GetUsers();
         public Task<User> GetUser(int id);

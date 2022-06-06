b95849fc201c5e728ad93c735b9b74a74efd3942
LaithAlamat <laithalamat4@gmail.com>
2022-06-06 13:04:12 +0300

    roles and identity v1.0

---
+++ E_Commerce/E_Commerce/Auth/Model/DTO/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Auth.Model.DTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public IList<string> Roles { get; internal set; }
+    }
+}

061ed31b07f91d1a98f0700ad70c38ec6232d8e6
OsamaAlzaghal <osama.alzaghal@gmail.com>
2022-06-28 22:28:30 +0300

    Editor role

--- E_Commerce/E_Commerce/Auth/UserService.cs
+++ E_Commerce/E_Commerce/Auth/UserService.cs
@@ -59,7 +59,7 @@ namespace E_Commerce.Models.Services
             {
                 // here goes the roles specifications ... 
                 IList<string> Roles = new List<string>();
-                Roles.Add("administrator");
+                Roles.Add("editor");
                 await _userManager.AddToRolesAsync(user, Roles);
                 await _email.WelcomeMail(registerDto.Email);
                 return new UserDTO
---
+++ E_Commerce/E_Commerce/Migrations/20220628192321_RemoveURL.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace E_Commerce.Migrations
+{
+    public partial class RemoveURL : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "URL",
+                value: null);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "URL",
+                value: "");
+        }
+    }
+}
